42a5409d10b85a714927658fd0758e15ea3ba3c8
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 12:02:32 +0300

    Task8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+double number1 = 0;
+double dev = 0;
+double index = 0;
+
+Console.WriteLine("Enter number: ");
+string userenter1 = Console.ReadLine();
+
+
+bool enter1 = double.TryParse(userenter1, out number1);
+if (enter1)
+{
+    while (index < number1)
+    {
+        if (enter1)
+        {
+            dev = index% 2;
+            if (dev == 0)
+            {
+                Console.Write($"{index},  ");
+            }
+        index++;
+        }
+        
+    }
+}
+if (!enter1)
+{
+    Console.Write("Wrong input");
+}
