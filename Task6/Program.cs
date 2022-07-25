94d7497aa96a689966290e4a1a572708e913144c
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 11:39:21 +0300

    Task6 done

---
+++ Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿double number1 = 0;
+double max = 0;
+
+Console.WriteLine("Enter number: ");
+string userenter1 = Console.ReadLine();
+
+
+bool enter1 = double.TryParse(userenter1, out number1);
+if (enter1)
+{
+    max = number1% 2;
+    if (max == 0)
+    {
+        Console.Write("True");
+    }
+    else
+    {
+        Console.Write("False");
+    }
+}
+if (!enter1)
+{
+    Console.Write("Wrong input");
+}
