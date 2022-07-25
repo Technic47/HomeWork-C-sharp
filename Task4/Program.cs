4296acb21bedb1d2a031f0a68fdf37155480013b
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 10:45:30 +0300

    Task4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,49 @@
+﻿int number1 = 0;
+int number2 = 0;
+int number3 = 0;
+int max = 0;
+
+Console.WriteLine("Enter number1: ");
+string userenter1 = Console.ReadLine();
+Console.WriteLine("Enter number2: ");
+string userenter2 = Console.ReadLine();
+Console.WriteLine("Enter number3: ");
+string userenter3 = Console.ReadLine();
+
+bool enter1 = int.TryParse(userenter1, out number1);
+if (enter1)
+{
+    max = number1;
+}
+if (!enter1)
+{
+    Console.Write("Wrong input");
+}
+
+bool enter2 = int.TryParse(userenter2, out number2);
+if (enter2)
+{
+    if (number2 > max)
+    {
+        max = number2;
+    }
+}
+if (!enter2)
+{
+    Console.Write("Wrong input");
+}
+
+bool enter3 = int.TryParse(userenter3, out number3);
+if (enter3)
+{
+    if (number3 > max)
+    {
+        max = number3;
+    }
+}
+if (!enter3)
+{
+    Console.Write("Wrong input");
+}
+
+Console.Write($"Max =  {max}");
